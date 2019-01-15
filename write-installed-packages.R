e448bd3fc728725f52b8bd9f5c1dc0e315a93c50
KellyAvery <kelly.mcginnity@gmail.com>
2019-01-15 18:13:58 -0500

    Write data to a file

--- write-installed-packages.R
+++ write-installed-packages.R
@@ -12,3 +12,11 @@ library(tidyverse)
 ipdf <- installed.packages() %>%
   as.tibble() %>%
   select(Package, LibPath, Version, Priority, Built)
+
+## write this data frame to data/installed-packages.csv
+## hint: readr::write_csv() or write.table()
+## idea: try using here::here() to create the file path
+
+library(here)
+ipdf %>%
+  write_csv(here::here("data", "installed-packages.csv"))
